f5c44f5e4e8c2ca00b6cad5661e094360fa829c6
imssyang <imssyang@gmail.com>
2022-08-09 16:20:08 +0800

    update env.sh

--- setup/env.sh
+++ setup/env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 export OPENGL_DISABLE_ENV=yes
+eval "optbin -s /opt/opengl/bin"
 eval "optlib -s /opt/opengl/lib/x86_64-linux-gnu"
 eval "optlib -s /opt/opengl/lib/x86_64-linux-gnu/dri"
 eval "optpkg -s /opt/opengl/lib/x86_64-linux-gnu/pkgconfig"
